0961caf2169a10b90399b627f31d89c92982fe1e
luxxyanjaymabar <110882963+luxxyanjaymabar@users.noreply.github.com>
2022-08-25 16:58:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 //Ini untuk setting Bot//
 
 //『 Utama 』 //
-global.owner = ['6282xxxxxx'] 
-global.mods = ['6282xxxxxx'] 
-global.prems = ['6282xxxxxx'] 
+global.owner = ['6288210072756'] 
+global.mods = ['6288210072756'] 
+global.prems = ['6288210072756'] 
 
 //『 Info Owner 』 //
-global.nameowner = '⫹⫺ Owner'
-global.numberowner = '6282xxxxxx'
-global.instagram = 'https://instagram.com/prm2.0'
-global.github = 'https://github.com/BOTCAHX'
-global.dana = '6282xxxxxx'
-global.pulsa = '6282xxxxxx'
-global.gopay = ''
+global.nameowner = '⫹⫺ LuxxyVFX'
+global.numberowner = '6288210072756'
+global.instagram = 'https://instagram.com/luxxyxyz'
+global.github = 'https://github.com/luxxyanjaymabar'
+global.dana = '6288210072756'
+global.pulsa = '6288210072756'
+global.gopay = '6288210072756'
 
 //『 Info Bot 』//
-global.namebot = 'R-txzyMd'
-global.gc = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
-global.web = 'https://myblogxyz.netlify.app' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'LuccyBot'
+global.gc = 'https://chat.whatsapp.com/CIv0mTBUteK8zy7crxdOj7'
+global.web = 'https://linktr.ee/luxxyxyz' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //『 Tampilan Dan Lainnya 』//
@@ -25,10 +25,10 @@ global.fotonya1 = 'https://botcahx-rest-api.herokuapp.com/api/textpro/marvel-stu
 global.fotonya2 = 'https://botcahx-rest-api.herokuapp.com/api/textpro/marvel-studios?text=List%20&text2=Menu' 
 global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© R-TXZY-MD'
+global.wm = '© LuccyBot'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
+global.wm2 = '⫹⫺ LuccyBot'
+global.wm3 = '© LuccyBot'
 global.wm4 = 'Ⓛ = Limit Ⓟ = Premium'
 global.htki = '––––––『' 
 global.htka = '』––––––'
@@ -40,7 +40,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.author = 'Bot WhatsApp\nLuccyBot'
 
 //『 Apikey 』 //
 global.APIs = { // API Prefix
